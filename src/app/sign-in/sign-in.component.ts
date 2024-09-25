5899c2449bf62d8fe7715ee054133de30b017a4a
Julia Kasenkova <goncharovajulia2904@gmail.com>
2024-09-25 17:27:52 +0300

    sign-up

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -2,15 +2,19 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { Router, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'sign-in',
     standalone: true,
     imports: [
+        RouterOutlet,
         MatInputModule,
         MatFormFieldModule,
         FormsModule,
-        ReactiveFormsModule 
+        ReactiveFormsModule,
+        RouterModule
     ],
     templateUrl: './sign-in.component.html',
     styleUrls: ['./sign-in.component.less'],
@@ -21,10 +25,15 @@ export class SignInComponent {
     signInForm: FormGroup;
     
     constructor(
+        private router: Router,
         private fb: FormBuilder) {
             this.signInForm = this.fb.group({
                 userName: '',
                 password:''
             });
+        }
+
+        registration(){
+            this.router.navigate(['/sign-up']);
         }
 }
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { RouterOutlet } from '@angular/router';
+import moment from 'moment-timezone';
+import { Timezone } from "../features/admin-panel/user-orientation/model/timezoneModel";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+@Component({
+    selector: 'sign-up',
+    standalone: true,
+    imports: [
+        MatInputModule,
+        MatFormFieldModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterOutlet,
+        MatAutocompleteModule,
+        MatCheckboxModule
+    ],
+    templateUrl: './sign-up.component.html',
+    styleUrls: ['./sign-up.component.less'],
+    encapsulation: ViewEncapsulation.None
+})
+
+export class SignUpComponent implements OnInit{
+    signUpForm: FormGroup;
+    timeZones: Timezone[] = [];
+    showPassword: boolean;
+    isAgree: boolean = false;
+
+    constructor(private fb: FormBuilder) {
+        this.signUpForm = this.fb.group({
+            email: ['', Validators.email],
+            name: ['', Validators.required],
+            surname: ['', Validators.required],
+            company: ['', Validators.required],
+            position: ['', Validators.required],
+            officeLocation: ['', Validators.required],
+            timeZoneControl: ['', Validators.required],
+            password: ['', Validators.compose([
+                Validators.required,
+                Validators.minLength(8),
+                Validators.maxLength(100)
+            ])],
+            confirmPassword: ['', Validators.compose([
+                Validators.required,
+                Validators.minLength(8),
+                Validators.maxLength(100)
+            ])]
+        });
+    }
+
+    ngOnInit(): void {
+        this.timeZones = moment.tz.names().map((zoneName) => {
+            return {
+                zoneName: zoneName,
+                utc: moment.tz(zoneName).format('Z'),
+                zoneAbbr: moment.tz(zoneName).zoneAbbr()
+            }
+        });
+    }
+}
