d85c5a708abf9aa2b3623a4b2b3eacb630c82d08
Julia Kasenkova <goncharovajulia2904@gmail.com>
2024-10-02 13:49:00 +0300

    Part 2. subscriptions management

--- src/app/subscriptions-management/subscriptions-management.component.ts
+++ src/app/subscriptions-management/subscriptions-management.component.ts
@@ -4,10 +4,9 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { RouterModule, RouterOutlet } from "@angular/router";
 import { Subscription } from "../models/subscription";
-import { Guid } from "guid-typescript";
-import { SubscriptionStatus } from "../models/subscriptionStatus";
 import { MatIconModule} from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { SubscriptionService } from "../services/subscriptionService";
 
 @Component({
     selector: 'subscriptions-management',
@@ -30,39 +29,13 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 export class SubscriptionManagementComponent implements OnInit {
     subscriptions: Subscription[];
     currentYear: number;
-    //for test
-    subscriptionsTmp: Subscription[] = [
-        {
-            id: Guid.parse('fd12a119-8ca5-4bc3-98b2-127dbcad94b1'),
-            timeZoneId: "Dublin, Kiev",
-            status: SubscriptionStatus.Active,
-            createdDate: "26/09/24",
-            activeHandovers: "4",
-            limitUsers: "10",
-            email: "jkasenkova@gmail.com"
-        },
-        {
-            id: Guid.parse('7afc81b6-4a52-4cdd-bcc6-87cd8d0e2b8b'),
-            timeZoneId: "Dublin, Kiev",
-            status: SubscriptionStatus.Active,
-            createdDate: "20/09/24",
-            activeHandovers: "3",
-            limitUsers: "15",
-            email: "user1@gmail.com"
-        },
-        {
-            id: Guid.parse('e53f1141-8383-4540-882a-b78c561e342e'),
-            timeZoneId: "Dublin, Kiev",
-            status: SubscriptionStatus.Blocked,
-            createdDate: "10/08/24",
-            activeHandovers: "2",
-            limitUsers: "20",
-            email: "user2@gmail.com"
-        }
-    ];
+
+    constructor(private subscriptionService: SubscriptionService){}
 
     ngOnInit(): void {
-        this.subscriptions = this.subscriptionsTmp;
+        this.subscriptionService.getSubscriptions().subscribe(subscriptions =>{
+            this.subscriptions = subscriptions
+        });
 
         this.currentYear = new Date().getFullYear();
     }
