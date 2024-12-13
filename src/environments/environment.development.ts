9044086470662ba96d4f4359d1f9d98bd07b3499
Petro Hlazunov <glazunov.peter@gmail.com>
2024-12-13 14:58:00 +0200

    +

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  routerUrl: "https://rw-dev-gateway.azurewebsites.net/api",
+  routerUrl: 'https://localhost:7209/api',
 
   appVersion: "0.0.0",
   
@@ -10,7 +10,7 @@ export const environment = {
       clientId: 'relayauth',
       scope: 'relayworks',
       secretId: 'fake-secret-id', // <Your auth secret id here>
-      authBaseUrl: 'https://rw-is-dev.azurewebsites.net',
+      authBaseUrl: 'https://localhost:5443',
       // keys to store tokens at local storage
       accessTokenKey: 'DoPS3ZrQjM',
       refreshTokenKey: 'nmlP8PW2nb',
